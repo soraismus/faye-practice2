114d61a3e16afd3b98a2bd6d593ab10439367087
M. H. Declup <tertius_lupus@yahoo.com>
2013-10-07 12:09:16 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+gem 'rails', '4.0.0'
+
+gem 'sqlite3', :group => :development
+
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
+end
+
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+
+gem 'faye'
+gem 'thin'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FayePractice2::Application.config.secret_key_base = '7f154d2f71f35c49e93e749e0b49a7d475648631b39c3915912e1a35a34c22de618066f25c803f6bebc9c77265c67967b165470bf5a93b439e22e5ec31a2574e'
